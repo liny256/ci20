f741ee59b5886e95d4d41264aca5a545c9b1cb0f
Lindy Lin <Lindy@LindydeMBP.lan1>
2020-01-28 09:00:54 -0500

    initial commit

--- observation/basic.js
+++ observation/basic.js
@@ -1,6 +1,25 @@
 var systemOfCL=document.getElementById('combination-lock');
-
-window.addEventListener('scroll', function BigTShowUpLate() {
-
-  
+function BigTitleShowUp(){
+  systemOfCL.classList.add('big-title-showup');
 }
+
+
+var systemD=document.getElementById('system-documentation');
+function SubTitleShowUp(){
+  systemD.classList.add('sub-title-showup');
+}
+
+
+var indexNav=document.getElementById('index-next');
+function IndexNextUp(){
+  indexNav.className +="next-goes-up";
+}
+
+
+
+function TitleShowUpLate(){
+  setTimeout(BigTitleShowUp,1000);
+  setTimeout(SubTitleShowUp,1000);
+  setTimeout(IndexNextUp,900);
+}
+TitleShowUpLate();
